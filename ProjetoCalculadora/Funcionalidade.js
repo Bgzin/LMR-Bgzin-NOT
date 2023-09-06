5f719af8fbfe153aa2ba0656c79d9d6e8bbe0ed5
Bgzin <rogerbiagioni06@gmail.com>
2023-09-05 21:33:28 -0300

    05/09 com comentarios

--- ProjetoCalculadora/Funcionalidade.js
+++ ProjetoCalculadora/Funcionalidade.js
@@ -1,7 +1,7 @@
 function dis(val) {
     document.getElementById("result").value += val
 }
-
+//basicamente a myFunctio espera o evento "event " ser desparado, no caso quando o cliente clicar em qualquer tecla.
 function myFunction(event) {
     if (event.key == '0' || event.key == '1'
         || event.key == '2' || event.key == '3'
@@ -13,6 +13,7 @@ function myFunction(event) {
         document.getElementById("result").value += event.key;
 }
 
+//aqui basicamente quando o usiario clicar enter no teclado vai funcionar 
 var cal = document.getElementById("calcu");
 cal.onkeyup = function (event) {
     if (event.keyCode === 13) {
@@ -23,14 +24,14 @@ cal.onkeyup = function (event) {
     }
 }
 
-// Function that evaluates the digit and return result
+
 function solve() {
-    let x = document.getElementById("result").value
-    let y = math.evaluate(x)
-    document.getElementById("result").value = y
+    let x = document.getElementById("result").value  //aqui o script le os numeros digitados / operaçoes inseridas na pagina HTML 
+    let y = math.evaluate(x) //aqui é onde a biblioteca math é usada para reconhecer as operaçoes matematicas utilizadas
+    document.getElementById("result").value = y //aqui o resultado do cálculo é atribuído ao campo de entrada com o ID "result
 }
 
-// Function that clear the display
+// aqui é basicamente uma atribuição para o clear, clicando nele "substitui" tudo oque foi digitado por vazio, fazendo sumir tudo ("clear").
 function clr() {
     document.getElementById("result").value = ""
 }
